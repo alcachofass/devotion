7b9aa5aca61d4223207234b67133fce8e72c54ba
Sancte Bruno, ora pro nobis <sanctebruno@gmail.com>
2023-02-19 00:02:31 -0300

    Disable FPS decorations

--- ratoa_gamecode/code/cgame/cg_draw.c
+++ ratoa_gamecode/code/cgame/cg_draw.c
@@ -5105,9 +5105,9 @@ static void CG_DrawBottomFPS( void ) {
 	color[0] = color[1] = color[2] = color[3] = 1.0;
 	color[3] = RSB4_DECOR_ALPHA;
 	trap_R_SetColor(color);
-	CG_DrawPic(x, y, w, h, cgs.media.bottomFPSShaderDecor );
+	//CG_DrawPic(x, y, w, h, cgs.media.bottomFPSShaderDecor );
 	trap_R_SetColor(NULL);
-	CG_DrawPic(x, y, w, h, cgs.media.bottomFPSShaderColor );
+	//CG_DrawPic(x, y, w, h, cgs.media.bottomFPSShaderColor );
 
 	if (fps == -1) {
 		return;
