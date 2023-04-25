22d8a82ab8d6c9c995a20f24e79653c95b478f81
alcachofass <32006541+alcachofass@users.noreply.github.com>
2023-04-25 06:29:40 -0700

    A more interesting cursor.

--- ratoa_gamecode/code/q3_ui/ui_qmenu.c
+++ ratoa_gamecode/code/q3_ui/ui_qmenu.c
@@ -1725,7 +1725,7 @@ void Menu_Cache( void )
 	uis.charsetProp		= trap_R_RegisterShaderNoMip( "menu/art/font1_prop.tga" );
 	uis.charsetPropGlow	= trap_R_RegisterShaderNoMip( "menu/art/font1_prop_glo.tga" );
 	uis.charsetPropB	= trap_R_RegisterShaderNoMip( "menu/art/font2_prop.tga" );
-	uis.cursor          = trap_R_RegisterShaderNoMip( "menu/art/3_cursor2" );
+	uis.cursor          = trap_R_RegisterShaderNoMip( "menu/art_blueish/3_cursor2" );
 	uis.rb_on           = trap_R_RegisterShaderNoMip( "menu/art/switch_on" );
 	uis.rb_off          = trap_R_RegisterShaderNoMip( "menu/art/switch_off" );
 
