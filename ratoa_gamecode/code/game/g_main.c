51d19d1b1218508078ca355f31de578aafd5d156
alcachofass <32006541+alcachofass@users.noreply.github.com>
2023-08-24 21:21:24 -0700

    Shrink output length of ServerInfo string.

--- ratoa_gamecode/code/game/g_main.c
+++ ratoa_gamecode/code/game/g_main.c
@@ -606,7 +606,7 @@ static cvarTable_t		gameCvarTable[] = {
         { &g_lagLightning, "g_lagLightning", "1", CVAR_ARCHIVE, 0, qtrue },
 //unlagged - server options
         { &g_ambientSound, "g_ambientSound", "0", CVAR_ARCHIVE, 0, qfalse },
-        { &g_rocketSpeed, "g_rocketSpeed", "900", CVAR_ARCHIVE | CVAR_SERVERINFO, 0, qtrue },
+        { &g_rocketSpeed, "g_rocketSpeed", "900", CVAR_ARCHIVE, 0, qtrue },
 	// TODO: CVAR_ARCHIVE
         { &g_maxExtrapolatedFrames, "g_maxExtrapolatedFrames", "2", 0 , 0, qfalse },
 
@@ -663,7 +663,7 @@ static cvarTable_t		gameCvarTable[] = {
 
         { &g_newShotgun, "g_newShotgun", "0", CVAR_ARCHIVE, 0, qtrue },
 
-	{ &g_movement,   "g_movement", "0", CVAR_ARCHIVE | CVAR_SERVERINFO, 0, qtrue },
+	{ &g_movement,   "g_movement", "0", CVAR_ARCHIVE, 0, qtrue },
 	{ &g_crouchSlide,   "g_crouchSlide", "0", CVAR_ARCHIVE, 0, qtrue },
 	{ &g_slideMode,   "g_slideMode", "0", CVAR_ARCHIVE, 0, qtrue },
 	{ &g_rampJump,     "g_rampJump", "0", CVAR_ARCHIVE, 0, qtrue },
@@ -692,7 +692,7 @@ static cvarTable_t		gameCvarTable[] = {
         { &g_killSafety, "g_killSafety", "500", CVAR_ARCHIVE , 0, qfalse },
         { &g_killDisable, "g_killDisable", "0", CVAR_ARCHIVE , 0, qfalse },
 
-        { &g_startWhenReady, "g_startWhenReady", "0", CVAR_ARCHIVE | CVAR_SERVERINFO, 0, qfalse },
+        { &g_startWhenReady, "g_startWhenReady", "0", CVAR_ARCHIVE, 0, qfalse },
         { &g_autoStartTime, "g_autoStartTime", "0", CVAR_ARCHIVE, 0, qfalse },
         { &g_autoStartMinPlayers, "g_autoStartMinPlayers", "0", CVAR_ARCHIVE, 0, qfalse },
 
@@ -824,7 +824,7 @@ static cvarTable_t		gameCvarTable[] = {
 	{ &g_grapple, "g_grapple", "0", CVAR_ARCHIVE, 0, qtrue },
 
 	//nexuiz style rocket arena
-	{ &g_rockets, "g_rockets", "0", CVAR_SERVERINFO | CVAR_LATCH | CVAR_NORESTART, 0, qfalse },
+	{ &g_rockets, "g_rockets", "0", CVAR_LATCH | CVAR_NORESTART, 0, qfalse },
 
 	//Instantgib and Vampire thingies
 	{ &g_instantgib, "g_instantgib", "0", CVAR_SERVERINFO | CVAR_LATCH, 0, qfalse },
@@ -833,7 +833,7 @@ static cvarTable_t		gameCvarTable[] = {
 	{ &g_regen, "g_regen", "0", CVAR_NORESTART, 0, qtrue },
 	{ &g_vampireMaxHealth, "g_vampire_max_health", "500", CVAR_NORESTART, 0, qtrue },
 	{ &g_lms_lives, "g_lms_lives", "1", CVAR_NORESTART, 0, qtrue },
-	{ &g_lms_mode, "g_lms_mode", "0", CVAR_SERVERINFO | CVAR_ARCHIVE | CVAR_NORESTART, 0, qtrue },
+	{ &g_lms_mode, "g_lms_mode", "0", CVAR_ARCHIVE | CVAR_NORESTART, 0, qtrue },
 
 	{ &g_coins, "g_coins", "0", CVAR_ARCHIVE, 0, qtrue },
 	{ &g_coinsDefault, "g_coinsDefault", "1", CVAR_ARCHIVE, 0, qtrue },
@@ -852,7 +852,7 @@ static cvarTable_t		gameCvarTable[] = {
         
         //KK-OAX
         { &g_sprees, "g_sprees", "sprees.dat", 0, 0, qfalse },
-        { &g_altExcellent, "g_altExcellent", "0", CVAR_SERVERINFO, 0, qtrue}, 
+        { &g_altExcellent, "g_altExcellent", "0", 0, 0, qtrue}, 
         { &g_spreeDiv, "g_spreeDiv", "5", 0, 0, qfalse},
         
         //Used for command/chat flooding
@@ -891,7 +891,7 @@ static cvarTable_t		gameCvarTable[] = {
 
 	//Devotion
 	{ &pmove_autohop, "pmove_autohop", "0", CVAR_SYSTEMINFO | CVAR_ARCHIVE, 0, qfalse},
-	{ &g_vulnerableMissiles, "g_vulnerableMissiles", "0", CVAR_SERVERINFO | CVAR_ARCHIVE | CVAR_NORESTART, 0, qfalse}	//mrd
+	{ &g_vulnerableMissiles, "g_vulnerableMissiles", "0", CVAR_ARCHIVE | CVAR_NORESTART, 0, qfalse}	//mrd
 };
 
 // bk001129 - made static to avoid aliasing
